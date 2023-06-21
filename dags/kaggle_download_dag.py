4531d139cad3863f3bd4b8b3f2e9670787551e30
Baptiste S <contact@baptistesauvecanne.fr>
2023-06-21 12:03:29 +0200

    Develop (#13)
    * feat: add data folder in .gitignore
    
    * refactor: remove kaggle file config, unsing environment variables
    
    * refactor: using the good folder to output path, use shedule insteed shedule_interval (deprecated)

--- dags/kaggle_download_dag.py
+++ dags/kaggle_download_dag.py
@@ -1,6 +1,3 @@
-import os
-import logging
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
@@ -17,12 +14,12 @@ default_args = {
     'start_date': datetime(2021, 1, 1),
 }
 
-with DAG('kaggle_download_dag', default_args=default_args, schedule_interval='@daily', catchup=False) as dag:
+with DAG('kaggle_download_dag', default_args=default_args, schedule='@daily', catchup=False) as dag:
     download_dataset = PythonOperator(
         task_id='download_dataset',
         python_callable=download_dataset,
         op_kwargs={
             'dataset': 'airbusgeo/airbus-aircrafts-sample-dataset',
-            'output_path': '/tmp/kaggle_dataset'
+            'output_path': '/opt/airflow/data/airbus-aircrafts-sample-dataset'
         }
     )
